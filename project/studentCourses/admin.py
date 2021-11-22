afda29a3b6947585d50abb453ca6ce3b337b3be5
青青子衿 <61900087+qingfusheng@users.noreply.github.com>
2021-11-22 13:28:50 +0800

    Delete project directory

--- project/studentCourses/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from studentCourses.models import MyCourse, AllCourse
-# Register your models here.
-
-admin.site.register(MyCourse)
-admin.site.register(AllCourse)
